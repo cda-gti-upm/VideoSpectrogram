86a2acd93eeb1857e7dda340c56589387c1c34b8
EnolAyo <enolayo@gmail.com>
2023-11-13 12:50:42 +0100

    primer commit

--- plot_seismic_data_plotly.py
+++ plot_seismic_data_plotly.py
@@ -24,17 +24,17 @@ from scipy.ndimage import uniform_filter1d
 import plotly.express as px
 import pandas as pd
 from screeninfo import get_monitors
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output, callback
+from datetime import date
 
-app = Dash(__name__)
 
 """
 Functions
 """
-def read_and_preprocessing(path_data, format_in, starttime, endtime):
-    """
-    Read, sort, merge, and filter data
-    """
+
+
+def read_and_preprocessing():
+
     # Read data
     print(f'Reading data ...')
     st = read_data_from_folder(path_data, format_in, starttime, endtime)
@@ -47,41 +47,38 @@ def read_and_preprocessing(path_data, format_in, starttime, endtime):
     # Merge traces
     print(f'Merging data ...')
     st.merge(method=0, fill_value=0)
-    tr = st[0]
+    trace = st[0]
 
     # Filtering 50 Hz
     if filter_50Hz_f:
         print(f'Filtering 50 Hz signal ...')
-        tr.data = obspy.signal.filter.bandstop(tr.data, 49, 51, tr.meta.sampling_rate, corners=8, zerophase=True)
+        trace.data = obspy.signal.filter.bandstop(trace.data, 49, 51, trace.meta.sampling_rate, corners=8, zerophase=True)
 
-    return tr
+    return trace
 
-def prepare_fig(tr, prefix_name, a_min, a_max):
+
+def prepare_fig(trace, start_time, end_time, prefix_name):
     print(f'Preparing figure...')
-
+    print('Updating dates...')
+    tr_x = trace.slice(start_time, end_time)
     # Decimation
     print(f'Decimation...')
-    num_samples = len(tr.data)
+    num_samples = len(tr_x.data)
+    print(f'{prefix_name} trace has {len(tr_x.data)} samples...')
     n_pixels = []
     for m in get_monitors():
         n_pixels.append(m.width)
 
     target_num_samples = max(n_pixels)
-    oversampling_factor = 100
+    print(f'Monitor horizontal resolution is {target_num_samples} pixels')
+    oversampling_factor = 10
     factor = int(num_samples / (target_num_samples * oversampling_factor))
     if factor > 1:
-        tr.decimate(factor, no_filter=True)  # No antialiasing filtering because of lack of stability due to large decimation factor
-    """ Two lengthy
-    fm = num_samples * 0.5
-    target_fm = target_num_samples * 0.5
-    corner_freq = 0.4 * target_num_samples  # [Hz] Note that Nyquist is 0.5 * target_fm
-    if corner_freq < fm / 2:  # To avoid ValueError
-        tr.filter('lowpass', freq=corner_freq, corners=10, zerophase=True)    
-    tr.interpolate(sampling_rate=target_fm, method='lanczos', a=20)
-    """
-
+        tr_x.decimate(factor, no_filter=True)  # No antialiasing filtering because of lack of stability due to large decimation factor
+        print(f'{prefix_name} trace reduced to {len(tr_x.data)} samples...')
     # Plotting and formatting
-    df = pd.DataFrame({'data': tr.data, 'times': tr.times('utcdatetime')})  # check for problems with date format
+    print(f'Plotting and formating {prefix_name}...')
+    df = pd.DataFrame({'data': tr_x.data, 'times': tr_x.times('utcdatetime')})  # check for problems with date format
     xlabel = "Date"
     ylabel = "Amplitude"
     title = f'{prefix_name} {tr.meta.network}, {tr.meta.station}, {tr.meta.location}, Channel {tr.meta.channel} '
@@ -91,7 +88,7 @@ def prepare_fig(tr, prefix_name, a_min, a_max):
     # Use the parameters [a_min, a_max] if data values are inside that range. If not use the min and max data
     # values.
 
-    min_val, max_val = np.percentile(tr.data, [0, 100])
+    min_val, max_val = np.percentile(tr_x.data, [0, 100])
     y_range = [min_val, max_val]
     if a_min <= min_val and a_max >= max_val:
         y_range = [a_min, a_max]
@@ -99,13 +96,8 @@ def prepare_fig(tr, prefix_name, a_min, a_max):
     fig = px.line(df, x="times", y="data", range_y=y_range, title=title, labels={'times': xlabel, 'data': ylabel})
     return fig
 
-# Main program
-if __name__ == "__main__":
 
-    """
-    Process input arguments given by the configuration file.
-    The configuration file can have several set of parameters for plotting different geophones and channels.
-    """
+def get_config():
     parser = argparse.ArgumentParser()
     parser.add_argument("conf_path")
     args = parser.parse_args()
@@ -114,65 +106,78 @@ if __name__ == "__main__":
         for par in yaml.safe_load_all(file):
             par_list.append(par)
 
-    """ 
-    Process every set of parameters 
-    """
-    for i, par in enumerate(par_list):
-        print(f'Processing parameter set {i+1} out of {len(par_list)}')
-
-        # Simplify variable names
-        path_data = par['paths']['path_data']
-        path_output = par['paths']['path_output']
-        starttime = par['date_range']['starttime']
-        endtime = par['date_range']['endtime']
-        filter_50Hz_f = par['filter']['filter_50Hz_f']
-        format_in = par['data_format']['format_in']
-        a_max = par['plotting']['a_max']
-        a_min = par['plotting']['a_min']
-        time_interval_one_row = par['day_plotting']['time_interval_one_row']
-        fig_format = par['fig_format']
-        verbose = par['verbose']
-
-        # Date preprocessing
-        if starttime:
-            starttime = UTCDateTime(starttime)
-        if endtime:
-            endtime = UTCDateTime(endtime)
-
-        """
-        Read, sort, merge, and filter data
-        """
-        tr = read_and_preprocessing(path_data, format_in, starttime, endtime)
-        tr_rsam = tr.copy()
-
-        """
-        Plot seismic data
-        """
-
-        fig = prepare_fig(tr, 'Plot', a_min, a_max)
-        fig.show()
-        del tr
-
-        """
-        Plot RSAM
-        """
-        # Computes RSAM
-        n_samples = int(tr_rsam.meta.sampling_rate*60*10)  # Amount to 10 minutes
-        tr_rsam.data = uniform_filter1d(abs(tr_rsam.data), size=n_samples)
-
-        # Prepare figure
-        fig_RSAM = prepare_fig(tr_rsam, 'RSAM', a_min=0, a_max=1000)
-        fig_RSAM.show()
-        del tr_rsam
+    return par
 
 
-        """
-        Day plot of seismic data
-        """
-        """
-        file_name = f'{path_output}/day_plot_{tr.meta.network}_{tr.meta.station}_{tr.meta.location}_{tr.meta.channel}_' \
-                    f'from {tr.stats.starttime.strftime("%d-%b-%Y at %H.%M.%S")} ' \
-                    f'until {tr.stats.endtime.strftime("%d-%b-%Y at %H.%M.%S")}.png'
-        # st.plot(type='dayplot', interval=interval_min, size=(4096, 2304))
-        st.plot(type='dayplot', interval=time_interval_one_row, size=(4096, 2304), outfile=f'{file_name}')
-        """
+par = get_config()
+
+# Simplify variable names
+
+path_data = par['paths']['path_data']
+path_output = par['paths']['path_output']
+starttime = par['date_range']['starttime']
+endtime = par['date_range']['endtime']
+filter_50Hz_f = par['filter']['filter_50Hz_f']
+format_in = par['data_format']['format_in']
+a_max = par['plotting']['a_max']
+a_min = par['plotting']['a_min']
+time_interval_one_row = par['day_plotting']['time_interval_one_row']
+fig_format = par['fig_format']
+verbose = par['verbose']
+
+if starttime:
+    starttime = UTCDateTime(starttime)
+if endtime:
+    endtime = UTCDateTime(endtime)
+
+tr = read_and_preprocessing()
+
+# Computes RSAM
+
+tr_rsam = tr.copy()
+n_samples = int(tr_rsam.meta.sampling_rate * 60 * 10)  # Amount to 10 minutes
+tr_rsam.data = uniform_filter1d(abs(tr_rsam.data), size=n_samples)
+
+starttime = tr.stats.starttime
+endtime = tr.stats.endtime
+
+# Creating app layout:
+
+app = Dash(__name__)
+app.layout = html.Div([
+    html.H1("Welcome to the seismic data visualizator"),
+    dcc.Input(
+        id='startdate',
+        type='text',
+        value=starttime.strftime("%Y-%m-%d %H:%M:%S")
+    ),
+    dcc.Input(
+        id='enddate',
+        type='text',
+        value=endtime.strftime("%Y-%m-%d %H:%M:%S")
+    ),
+    dcc.Graph(id='time_plot'),
+    dcc.Graph(id='RSAM')
+])
+
+
+@app.callback(
+    Output('time_plot', 'figure'),
+    Output('RSAM', 'figure'),
+    Input('startdate', 'value'),
+    Input('enddate', 'value')
+)
+def update(startdate, enddate):
+    start_time = UTCDateTime(startdate)
+    end_time = UTCDateTime(enddate)
+    fig1 = prepare_fig(trace=tr, start_time=start_time, end_time=end_time, prefix_name='Plot')
+
+    fig2 = prepare_fig(trace=tr_rsam, start_time=start_time, end_time=end_time, prefix_name='RSAM')
+    print('Graph updated!')
+    return fig1, fig2
+
+
+# Main program
+
+if __name__ == "__main__":
+    app.run(debug=False)
